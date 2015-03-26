24d2cd4498f8e137032151024a28e65bcf005f2c
Dave Jachimiak <dave.jachimiak@gmail.com>
2015-03-26 09:04:50 -0400

    Watch only Haskell files

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import System.FSNotify
+import Filesystem.Path.CurrentOS (encodeString, decodeString)
 import System.Process (rawSystem)
-import Control.Concurrent (threadDelay)
+import System.Environment (getArgs)
 import Control.Monad (forever)
+import Text.Regex (mkRegex, matchRegex)
 
-main =
+main = do
+  (rawDir:command:_) <- getArgs
+
+  let dir = decodeString rawDir
+
   withManager $ \mgr -> do
-    watchDir
-      mgr          -- manager
-      "."          -- directory to watch
-      (const True) -- predicate
-      (\x -> print x) -- action
+    _ <- watchDir mgr dir isHaskellFile $ runCommand command
 
-    -- sleep forever (until interrupted)
-    forever $ threadDelay maxBound
+    forever getLine
+
+runCommand :: String -> Event -> IO ()
+runCommand command _ = do
+    rawSystem command []
+    print $ "doing" ++ command
+
+isHaskellFile :: Event -> Bool
+isHaskellFile (Added file _) = isHaskellFile' $ encodeString file
+isHaskellFile (Modified file _) = isHaskellFile' $ encodeString file
+isHaskellFile (Removed file _) = isHaskellFile' $ encodeString file
+
+isHaskellFile' :: String -> Bool
+isHaskellFile' file = 
+    case (matchRegex (mkRegex "hs$") file) of 
+      Just _ -> True
+      Nothing -> False
