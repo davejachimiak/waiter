e74ccb0198a3b5d9b6eae292d3ef31ab522256c0
Dave Jachimiak <dave.jachimiak@gmail.com>
2015-03-25 21:21:34 -0400

    Put in a program

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,17 @@
-main = do
-    print "hi"
+{-# LANGUAGE OverloadedStrings #-}
+
+import System.FSNotify
+import System.Process (rawSystem)
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
+
+main =
+  withManager $ \mgr -> do
+    watchDir
+      mgr          -- manager
+      "."          -- directory to watch
+      (const True) -- predicate
+      (\x -> print x) -- action
+
+    -- sleep forever (until interrupted)
+    forever $ threadDelay maxBound
