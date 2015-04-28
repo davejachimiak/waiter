b28534e1ce56a6aef74a451a4c67b6522b25e154
Dave Jachimiak <dave.jachimiak@gmail.com>
2015-04-27 21:05:11 -0400

    Revert "Remove unnecessary build process tracking"
    This reverts commit 8419aa082484a5d0d8f73d398b180c89c80c7d13.

--- src/Waiter.hs
+++ src/Waiter.hs
@@ -2,53 +2,74 @@ module Waiter (run) where
 
 import Filesystem.Path.CurrentOS (decodeString)
 import System.FSNotify (withManager, watchTree, Event)
-import System.Process (waitForProcess, terminateProcess, spawnCommand, callCommand)
+import System.Process (waitForProcess, terminateProcess, spawnCommand)
 import System.Process.Internals (ProcessHandle(..))
-import Control.Monad (forever, unless)
+import Control.Monad (forever, when, unless)
 import Control.Concurrent.MVar
 import Control.Concurrent (threadDelay)
 import Text.Regex (mkRegex, matchRegex)
 import Data.List (delete)
 
-import Waiter.Types 
-import Waiter.FileMatcher (fileDoesMatch)
+import Waiter.Types
 
 run :: CommandLine -> IO ()
 run commandLine = do
     let regexToWatch = fileRegex commandLine
         dirToWatch = decodeString $ dir commandLine
 
+    buildsState <- newMVar []
     blockState <- newMVar False
     serverProcess <- newEmptyMVar
 
-    buildAndServe commandLine serverProcess
+    buildAndServe commandLine buildsState serverProcess
 
     withManager $ \mgr -> do
         watchTree
             mgr
             dirToWatch
             (fileDoesMatch regexToWatch)
-            $ blockBuildAndServe commandLine blockState serverProcess
+            $ blockBuildAndServe commandLine blockState buildsState serverProcess
 
         forever getLine
 
-buildAndServe :: CommandLine -> MVar ProcessHandle -> IO ()
-buildAndServe commandLine serverProcess = do
-    callCommand $ buildCommand commandLine
-    stopServer serverProcess
-    startServer commandLine serverProcess
+buildAndServe :: CommandLine
+              -> MVar [ProcessHandle]
+              -> MVar ProcessHandle
+              -> IO ()
+buildAndServe commandLine buildsState serverProcess = do
+    currentBuilds <- build (buildCommand commandLine) buildsState
+
+    when (null currentBuilds)
+        $ stopServer serverProcess
+        >> startServer commandLine serverProcess
+
+build :: String -> MVar [ProcessHandle] -> IO [ProcessHandle]
+build buildCommand buildsState = do
+    buildProcess <- spawnCommand buildCommand
+    existingBuilds <- readMVar buildsState
+
+    swapMVar buildsState $ buildProcess : existingBuilds
+    waitForProcess buildProcess
+
+    buildsStateAfterBuild <- readMVar buildsState
+
+    let buildProcesses = delete buildProcess buildsStateAfterBuild
+
+    swapMVar buildsState buildProcesses
+    return buildProcesses
 
 blockBuildAndServe :: CommandLine
                    -> MVar Bool
+                   -> MVar [ProcessHandle]
                    -> MVar ProcessHandle
                    -> Event
                    -> IO ()
-blockBuildAndServe commandLine blockState serverProcess _ = do
+blockBuildAndServe commandLine blockState buildsState serverProcess _ = do
     doBlock <- readMVar blockState
 
     unless doBlock
         $ blockBatchEvents blockState
-        >> buildAndServe commandLine serverProcess
+        >> buildAndServe commandLine buildsState serverProcess
 
 blockBatchEvents :: MVar Bool -> IO Bool
 blockBatchEvents blockState = do
--- src/Waiter/Types.hs
+++ src/Waiter/Types.hs
@@ -1,7 +1,31 @@
 module Waiter.Types where
 
+import Text.Regex (mkRegex, matchRegex)
+import qualified Filesystem.Path.CurrentOS as OS (FilePath, encodeString) 
+import System.FSNotify (Event(..))
+import System.Process.Internals (ProcessHandle(..))
+
 data CommandLine = CommandLine
     { serverCommand :: String
     , buildCommand :: String
     , fileRegex :: String
     , dir :: String }
+
+instance Eq ProcessHandle where
+    (ProcessHandle handle1 _) == (ProcessHandle handle2 _) = handle1 == handle2
+
+class FileDoesMatch a where
+    fileDoesMatch :: String -> a -> Bool
+
+instance FileDoesMatch Event where
+    fileDoesMatch regex (Added filePath _) = fileDoesMatch' regex filePath
+    fileDoesMatch regex (Modified filePath _) = fileDoesMatch' regex filePath
+    fileDoesMatch regex (Removed filePath _) = fileDoesMatch' regex filePath
+
+fileDoesMatch' :: String -> OS.FilePath -> Bool
+fileDoesMatch' regex filePath =
+    let fileName = OS.encodeString filePath
+
+    in case matchRegex (mkRegex regex) fileName of
+        Just _ -> True
+        Nothing -> False
